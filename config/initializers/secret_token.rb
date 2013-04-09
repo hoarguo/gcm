954bac10696fc5ba473622c5c2912b114889cde1
hoarguo <hoarguo@gmail.com>
2013-04-09 17:45:15 +0800

    Create a rails application for test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phonebook::Application.config.secret_token = 'f89e1f42ccc578f81e0f4e9d3ae81d16161677b384a88a31bd12bd76e15f5991e69e168bbb603d25bcdf1cc3b82840988e1ff050a26ea1bdc1a75593caa3acc4'
